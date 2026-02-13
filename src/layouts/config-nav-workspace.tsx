69e0de7e3d33beee0b0eceaed91386c5b2337300
ezzi97 <ezeddin.eddaouy@gmail.com>
2026-02-13 20:13:01 +0100

    fix: [SER-5412] fix small bugs

--- src/layouts/config-nav-workspace.tsx
+++ src/layouts/config-nav-workspace.tsx
@@ -3,7 +3,7 @@
 export const _workspaces = [
   {
     id: 'team-1',
-    name: 'Gruppo Volontari del Soccorso',
+    name: 'Servizi Secondari & Sportivi',
     logo: `/assets/icons/workspaces/logo-1.webp`
   }
 ];
